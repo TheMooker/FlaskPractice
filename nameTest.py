ed8e06a636d6318b9fea90d68344e578a4add196
adabs <goddemraw@gmail.com>
2022-07-20 20:52:47 -0400

    More notes

--- nameTest.py
+++ nameTest.py
@@ -2,10 +2,18 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+# Simple route for showing a 'Hello!' message
 # @app.route('/')
 # def index():
 #     return '<h1>Hello!</h1>'
 
+
+# Grabs the request header, User-Agent, and displays the info it contains on the root url page.
+# "The User-Agent request header is a characteristic string that lets servers and network peers identify the application, 
+# operating system, vendor, and/or version of the requesting user agent."
+# https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/User-Agent
+# request cannot be global since in a multithreaded server, multiple requests from multiple clients are happening simultaneously.
+# each thread must see a different object in request.
 @app.route('/')
 def index():
     user_agent = request.headers.get('User-Agent')
